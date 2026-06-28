eec7f6a2d02b0256f8171a67c1e402e50bb05e79
Saksham977 <114497141+Saksham977@users.noreply.github.com>
2026-06-28 23:08:13 +0530

    Update comments

--- CommiIQ_app.js
+++ CommiIQ_app.js
@@ -1,9 +1,9 @@
 // Configure PDF.js Worker
 pdfjsLib.GlobalWorkerOptions.workerSrc =
 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js';
-// ==========================================
+// ===========================
 // Application State
-// ==========================================
+// ===========================
 let appState = {
     extractedRows: [],
     countdownSeconds: 1260,
